dd6cf66b9cf0ca5c94bab4bff4eb42d511ee21c2
Henry <henrysavit@gmail.com>
2026-02-08 16:57:59 -0500

    Update speed-test SHA for rebuilt v1.2.0

--- Casks/speed-test.rb
+++ Casks/speed-test.rb
@@ -1,6 +1,6 @@
 cask "speed-test" do
   version "1.2.0"
-  sha256 "d971ad0ba27a41126f5c10f3bd4005b649e32153b53625b936af1029a708cdbc"
+  sha256 "e14cd2d38afb3025511fb6d6bd99e5cee7eb8926b123ceca94d7c130388cab9d"
 
   url "https://github.com/hsavit1/SpeedTest/releases/download/v#{version}/SpeedTest-v#{version}.dmg"
   name "Speed Test"
