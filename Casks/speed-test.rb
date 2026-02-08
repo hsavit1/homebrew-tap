c6d120e928b4860180ae0c59058c9c4847af4c41
Henry <henrysavit@gmail.com>
2026-02-08 16:36:15 -0500

    Update speed-test to v1.2.0
    Add auto-poll toggle (every 30s)

--- Casks/speed-test.rb
+++ Casks/speed-test.rb
@@ -1,6 +1,6 @@
 cask "speed-test" do
-  version "1.1.0"
-  sha256 "b8bb04b6d23f5b62279dc77d0689364c1ae3c74e207fca7d354e9c7899a0ebde"
+  version "1.2.0"
+  sha256 "d971ad0ba27a41126f5c10f3bd4005b649e32153b53625b936af1029a708cdbc"
 
   url "https://github.com/hsavit1/SpeedTest/releases/download/v#{version}/SpeedTest-v#{version}.dmg"
   name "Speed Test"
@@ -9,7 +9,7 @@ cask "speed-test" do
 
   depends_on macos: ">= :sonoma"
 
-  app "Speed Test.app"
+  app "SpeedTest.app"
 
   zap trash: [
     "~/Library/Preferences/com.speedtest.app.plist",
